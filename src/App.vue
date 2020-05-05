7281d0bdb58de29cfa30fe6913cb7d77245056c8
TheFunkyMonk <rjlacount@gmail.com>
2020-05-04 23:00:26 -0700

    Add placeholder filter bar

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,18 @@
       rel="stylesheet"
     />
     <div class="p-4 md:p-8 bg-orange-200">
-      <div class="rounded-lg bg-green-500 max-w-3xl mx-auto px-8 py-px antialiased min-h-screen">
-        <h1
-          class="text-3xl md:text-4xl font-black text-white my-8 md:my-12 border-b border-white"
-        >{{ title }}</h1>
+      <div class="rounded-lg bg-green-500 max-w-3xl mx-auto px-8 py-2 antialiased min-h-screen">
+        <h1 class="text-3xl md:text-4xl font-black text-white my-8">{{ title }}</h1>
 
-        <div class="mb-8 text-white text-xs">
-          <p class="text-xs uppercase tracking-wider font-black">Filters</p>
-          <p class>Coming soon!</p>
+        <div class="flex flex-wrap bg-green-800 text-white rounded-lg px-4 py-px mb-8 text-xs">
+          <div class="w-1/2 px-2 my-4">
+            <p class="text-xs uppercase tracking-wider font-black">Filter</p>
+            <p class>Coming soon!</p>
+          </div>
+          <div class="w-1/2 px-2 my-4">
+            <p class="text-xs uppercase tracking-wider font-black">Sort</p>
+            <p class>Coming soon!</p>
+          </div>
         </div>
 
         <div class="flex flex-wrap -mx-4">
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="my-4 md:my-8 block shadow-lg rounded-lg overflow-hidden">
+  <div
+    class="my-4 md:my-8 block shadow-lg rounded-lg overflow-hidden transform hover:-translate-y-1 hover:shadow-2xl transition duration-150 cursor-pointer"
+  >
     <div class="px-4 bg-blue-200 p-px flex items-center justify-start">
       <img v-if="item.image" v-bind:src="item.image" v-bind:alt="item.name" class="w-8 -ml-2 mr-1" />
       <h2 class="text-base font-medium my-2">{{ item.name }}</h2>
