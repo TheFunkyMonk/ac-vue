bd3938152da3d9a4000412ec89b75c6fe579286c
TheFunkyMonk <rjlacount@gmail.com>
2020-05-05 23:40:43 -0700

    Add vue-gtag

--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import './assets/tailwind.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPlus, faMinus, faCaretDown, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import VueGtag from "vue-gtag";
 
 library.add(faPlus, faMinus, faCaretDown, faCheckCircle)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(VueGtag, {
+  config: { id: "UA-22060307-2" }
+})
 
 Vue.config.productionTip = false
 
